22b1a37bcacfbd2deb6cb1b269a1acfc6af46f11
Rodolfo <rodolfoafl@gmail.com>
2018-08-08 16:15:32 -0300

    Initial commit

---
+++ Assets/_Scripts/CanvasManager.cs
@@ -0,0 +1,90 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System.IO;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class CanvasManager : MonoBehaviour {
+
+	private Player[] _players;
+    private Player _newPlayer = new Player();
+
+    [SerializeField] RectTransform panel;
+
+    [SerializeField] RectTransform panelScore;
+    [SerializeField] Text textScoreboard;
+	//[SerializeField] Text textPrefab;
+
+	[SerializeField] InputField nameInput;
+	[SerializeField] InputField scoreInput;
+    [SerializeField] InputField passwordInput;
+
+    [SerializeField] Text errorText;
+    [SerializeField] Text scoreSentText;
+
+    void Start() {
+        errorText.enabled = false;
+        panelScore.gameObject.SetActive(false);
+    }
+
+	public void UpdatePanel(){
+        ResetScoreboardText();
+
+		_players = Player.GetPlayerData ();
+
+        int i = 1;
+
+        foreach(Player p in _players)
+        {
+            var line = i + " - " + p.name + " - " + p.score;
+            textScoreboard.text += line + "\n";
+            i++;
+        }
+	}
+
+    public void ResetScoreboardText()
+    {
+        textScoreboard.text = "";
+    }
+
+    public void Login()
+    {
+        _newPlayer.name = nameInput.text;
+        _newPlayer.password = passwordInput.text;
+
+        string toJson = JsonUtility.ToJson(_newPlayer);
+
+        //string loginReturn = Player.LoginPlayer(toJson);
+
+        if (!Player.LoginPlayer(toJson))
+        {
+            errorText.enabled = true;
+        }
+        else
+        {
+            //_newPlayer.name = loginReturn;
+            errorText.enabled = false;
+            panelScore.gameObject.SetActive(true);
+        }
+    }
+
+    public void SendScore(){
+        _newPlayer.password = "0";
+        _newPlayer.score = scoreInput.text;
+
+		string toJson = JsonUtility.ToJson (_newPlayer);
+        Debug.Log(toJson);
+
+		if(!Player.CreatePlayer(toJson))
+        {
+            scoreSentText.text = "Erro ao enviar pontuação!";
+        }
+        else
+        {
+            scoreSentText.text = "Pontuação enviada com sucesso!";
+        }
+        scoreSentText.enabled = true;
+	}
+
+
+}
---
+++ Assets/_Scripts/Player.cs
@@ -0,0 +1,103 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System.Net;
+using System.IO;
+
+[System.Serializable]
+public class Player {
+
+	public int id;
+	public string name;
+    public string password;
+	public string score;
+
+	//public Player[] _players;
+
+	public static Player[] GetPlayerData(){
+		HttpWebRequest request = (HttpWebRequest)WebRequest.Create("http://localhost/php_rest/api/player/read.php");
+		HttpWebResponse response = (HttpWebResponse)request.GetResponse();
+		StreamReader reader = new StreamReader(response.GetResponseStream());
+		string jsonResponse = reader.ReadToEnd();
+
+		Player[] players = JsonHelper.FromJson<Player> (jsonResponse);
+		return players;
+
+		//Player p = JsonUtility.FromJson<Player>(jsonResponse);
+		//return p;
+
+		/*foreach (Player p in _players) {
+			Debug.Log ("Player Name: " + p.name);
+			Debug.Log ("Player Score: " + p.score);
+		}*/
+	}
+
+	public static bool CreatePlayer(string json){
+		HttpWebRequest request = (HttpWebRequest)WebRequest.Create("http://localhost/php_rest/api/player/create.php");
+		request.Method = "POST";
+		request.ContentType = "application/json";
+
+		var streamWriter = new StreamWriter (request.GetRequestStream ());
+		streamWriter.Write(json.ToCharArray());
+		streamWriter.Close ();
+
+		HttpWebResponse response = (HttpWebResponse)request.GetResponse();
+		var reader = new StreamReader(response.GetResponseStream());
+		var textResponse = reader.ReadToEnd ();
+
+        Debug.Log(textResponse);
+        if (textResponse.Contains("Not"))
+        {
+            return false;
+        }
+        else
+        {
+            return true;
+        }
+		
+	}
+
+    public static bool LoginPlayer(string json)
+    {
+        HttpWebRequest request = (HttpWebRequest)WebRequest.Create("http://localhost/php_rest/api/player/login.php");
+        request.Method = "POST";
+        request.ContentType = "application/json";
+
+        var streamWriter = new StreamWriter(request.GetRequestStream());
+        streamWriter.Write(json.ToCharArray());
+        streamWriter.Close();
+
+        HttpWebResponse response = (HttpWebResponse)request.GetResponse();
+        var reader = new StreamReader(response.GetResponseStream());
+        var textResponse = reader.ReadToEnd();
+
+        Debug.Log(textResponse);
+        
+        if (textResponse.Contains("error"))
+        {
+            Debug.Log("Erro no Login");
+            return false;
+        }
+        else
+        {
+            return true;
+        }
+    }
+
+    /*static string GetFullName(string response) {
+        var begin = response.LastIndexOf("contactFullName\":\"");
+        var end = response.IndexOf("\",\"contactLID");
+        var text = response.Substring(begin, end - begin);
+
+        begin = text.LastIndexOf("\"");
+        begin++;
+       
+        var name = text.Substring(begin, text.Length - begin);
+        return name;
+        //Debug.Log(name);
+    }*/
+
+
+
+
+}
